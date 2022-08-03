52793558ee142bde6b71dda0b07e55e35ff5224d
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-08-03 15:08:53 -0500

    [FEATURES] test

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ContainerHeader} from './HeaderStyles';
+
+const Header = () => {
+  return (
+    <ContainerHeader>
+        <h1>Renta car</h1>
+        <button>Iniciar Sesion</button>
+        <button>Crear Cuenta</button>
+    </ContainerHeader>
+  )
+}
+
+export default Header
