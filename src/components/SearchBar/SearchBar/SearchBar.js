ba849d7f4f0cad1db1270b40ba9f65a7d1710c00
davidcastellanos <castellanos.johann@gmail.com>
2022-08-03 15:44:28 -0500

    [test] test files

---
+++ src/components/SearchBar/SearchBar/SearchBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SearchBar(params) {
+  return <div></div>;
+}
+
+export default SearchBar;
---
+++ src/components/SearchBar/SearchSection.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SearchBar from './SearchBar/SearchBar';
+
+function SearchSection(params) {
+  return 'HOla';
+}
+
+export default SearchSection;
