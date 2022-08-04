e2847ff69f6642cd71de3e018767b894e6021159
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-08-04 15:14:24 -0500

    [UPDATE] theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import "./App.css";
+import Layout from './components/Layout/Layout';
+import SearchSection from './components/SearchBar/SearchSection';
+import {Theme} from './Theme';
+import { ThemeProvider } from 'styled-components';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className="App">
+    <ThemeProvider theme={Theme} >
+      <Layout>
+          <SearchSection />
+        </Layout>
+    </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/components/header/HeaderStyles.js
+++ src/components/header/HeaderStyles.js
@@ -4,7 +4,7 @@ export const ContainerHeader = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    background-color: black;
+    background-color: ${({theme}) => theme.background};
     color: white;
 `
 
@@ -15,7 +15,7 @@ export const Buttons = styled.button`
     font-size: 18px;
     color: white;
     border-radius: 5px;
-    background-image: linear-gradient(to right, #FCA311 50%, #E5E5E5 50%);
+    background-image: linear-gradient(to right, ${({theme}) => theme.secondary} 50%, ${({theme}) => theme.third} 50%);
     background-size: 200%;
     background-repeat: no-repeat;
     background-position: left;
