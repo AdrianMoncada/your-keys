a0e27604df837d08a71ba9aa47e6eb3ef467894b
Beatu0429 <nievesb0429@gmail.com>
2022-08-29 20:17:14 -0500

    [UPDATE] fixing errors

--- src/main/java/com/example/proyectoG8/model/dto/CharacteristicDTO.java
+++ src/main/java/com/example/proyectoG8/model/dto/CharacteristicDTO.java
@@ -3,6 +3,7 @@ package com.example.proyectoG8.model.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import java.io.Serializable;
 import java.util.List;
 
@@ -12,11 +13,11 @@ public class CharacteristicDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private Long id;
+    private Long idCharacteristic;
 
+    private String nameCharacteristic;
 
-    private String name;
-
+    private String value;
 
     private String icon;
 
