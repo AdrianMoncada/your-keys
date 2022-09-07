3372358d3d0971e2f7b9355ab516477e23db5cb9
Beatu0429 <nievesb0429@gmail.com>
2022-09-07 14:26:35 -0500

    [UPDATE] fixing typo in vehicle repository

--- src/main/java/com/example/proyectoG8/repository/IVehicleRepository.java
+++ src/main/java/com/example/proyectoG8/repository/IVehicleRepository.java
@@ -13,11 +13,11 @@ public interface IVehicleRepository extends JpaRepository<Vehicle, Long> {
 
     @Query(value = "select * from vehicles where city_id= :cityId and id_vehicle not in " +
             "(select vehicle_id from bookings where initialdate between :initialDate and " +
-            ":finalDate or final_date between :initialdate and :final_date group by vehicle_id)", nativeQuery = true)
+            ":finalDate or final_date between :initialDate and :finalDate group by vehicle_id)", nativeQuery = true)
     public List<Vehicle> findByDateAndCity(LocalDate initialDate, LocalDate finalDate, Long cityId);
 
     @Query(value = "select * from vehicles where id_vehicle not in " +
             "(select vehicle_id from bookings where initialdate between :initialDate and " +
-            ":finalDate or final_date between :initialdate and :final_date group by vehicle_id)", nativeQuery = true)
+            ":finalDate or final_date between :initialDate and :finalDate group by vehicle_id)", nativeQuery = true)
     public List<Vehicle> findByDate(LocalDate initialDate, LocalDate finalDate);
 }
